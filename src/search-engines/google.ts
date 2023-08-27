d32a46b17233b272a1f6b59a3d22c9b349bddfe7
William Huebner <wilhueb@gmail.com>
2023-08-27 06:43:16 -0400

    fix: remove template from Google searchUrl

--- src/search-engines/google.ts
+++ src/search-engines/google.ts
@@ -1,5 +1,5 @@
 export default {
-  searchUrl: "https://www.google.com/search?q=%s",
+  searchUrl: "https://www.google.com/search?q=",
   compUrl:
     "https://www.google.com/complete/search?client=chrome-omni&gs_ri=chrome-ext&oit=1&pgcl=7&q=%s",
   compFn: (res: { text: string }) => JSON.parse(res.text)[1],
