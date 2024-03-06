916138ad75a6af0f448f48452735e76daf5b14e6
William Huebner <wilhueb@gmail.com>
2024-03-06 02:51:20 -0500

    lol wiki

--- src/search-engines/index.ts
+++ src/search-engines/index.ts
@@ -1,7 +1,8 @@
 import github from './github';
 import google from './google';
 import imdb from './imdb';
+import lolwiki from './lolwiki';
 import wikipedia from './wikipedia';
 import youtube from './youtube';
 
-export default [github, google, imdb, wikipedia, youtube];
+export default [github, google, imdb, lolwiki, wikipedia, youtube];
