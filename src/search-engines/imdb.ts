dad3111a037ae8cd35fec709937453201ebab07b
William Huebner <wilhueb@gmail.com>
2024-03-08 00:48:55 -0500

    style: prettier format

--- src/search-engines/imdb.ts
+++ src/search-engines/imdb.ts
@@ -1,55 +1,55 @@
 import { SearchEngine } from 'src/models';
 
 type ImdbCompResult = {
-    d: {
-        i: {
-            height: number;
-            imageUrl: string;
-            width: number;
-        };
-        id: string;
-        l: string;
-        rank: number;
-        s: string;
-        y?: number;
-    }[];
+  d: {
+    i: {
+      height: number;
+      imageUrl: string;
+      width: number;
+    };
+    id: string;
+    l: string;
+    rank: number;
+    s: string;
+    y?: number;
+  }[];
 };
 
 const engine: SearchEngine = {
-    alias: 'i',
-    name: 'imdb',
-    searchUrl: 'https://www.imdb.com/find?q=%s',
-    compUrl: 'https://v3.sg.media-imdb.com/suggestion/x/%s.json?includeVideos=0',
-    compFn: (res) =>
-        (JSON.parse(res.text) as ImdbCompResult).d
-            .filter((item) => !item.id.startsWith('/')) // remove unrelated ads
-            .map((item) => {
-                let title = item.l;
-                if (item.y !== undefined) title += ` (${item.y})`;
+  alias: 'i',
+  name: 'imdb',
+  searchUrl: 'https://www.imdb.com/find?q=%s',
+  compUrl: 'https://v3.sg.media-imdb.com/suggestion/x/%s.json?includeVideos=0',
+  compFn: (res) =>
+    (JSON.parse(res.text) as ImdbCompResult).d
+      .filter((item) => !item.id.startsWith('/')) // remove unrelated ads
+      .map((item) => {
+        let title = item.l;
+        if (item.y !== undefined) title += ` (${item.y})`;
 
-                let url = 'https://imdb.com';
-                if (item.id.startsWith('tt')) url += `/title/${item.id}/`;
-                else if (item.id.startsWith('nm')) url += `/name/${item.id}/`;
-                else throw new Error(`unknown imdb id: ${item}`);
+        let url = 'https://imdb.com';
+        if (item.id.startsWith('tt')) url += `/title/${item.id}/`;
+        else if (item.id.startsWith('nm')) url += `/name/${item.id}/`;
+        else throw new Error(`unknown imdb id: ${item}`);
 
-                const html = `
-                    <div style="display: flex; flex-direction: row">
-                      <img
-                        style="max-width: 160px; height: 90px; margin-right: 0.8em"
-                        alt="thumbnail"
-                        src="${item.i.imageUrl}">
-                      <div>
-                        <div class="title">${title}</div>
-                        <div>
-                          <div>${item.s}</div>
-                          <div class="url">${url}</div>
-                        </div>
-                      </div>
-                    </div>`;
+        const html = `
+          <div style="display: flex; flex-direction: row">
+            <img
+              style="max-width: 160px; height: 90px; margin-right: 0.8em"
+              alt="thumbnail"
+              src="${item.i.imageUrl}">
+            <div>
+              <div class="title">${title}</div>
+              <div>
+                <div>${item.s}</div>
+                <div class="url">${url}</div>
+              </div>
+            </div>
+          </div>`;
 
-                return { html, props: { url } };
-            }),
-    faviconUrl: 'https://www.imdb.com/favicon.ico',
+        return { html, props: { url } };
+      }),
+  faviconUrl: 'https://www.imdb.com/favicon.ico',
 };
 
 export default engine;
--- src/search-engines/lolwiki.ts
+++ src/search-engines/lolwiki.ts
@@ -1,48 +1,48 @@
 import { SearchEngine } from 'src/models';
 
 type CompResult = {
-    query: string;
-    suggestions: string[];
-    ids: Record<CompResult['suggestions'][number], number>;
-    images: Record<CompResult['ids'][string], string | null>;
+  query: string;
+  suggestions: string[];
+  ids: Record<CompResult['suggestions'][number], number>;
+  images: Record<CompResult['ids'][string], string | null>;
 };
 
 const engine: SearchEngine = {
-    alias: 'lol',
-    name: 'league of legends wiki',
-    searchUrl: 'https://leagueoflegends.fandom.com/wiki/Special:Search?scope=internal&query=%s',
-    compUrl:
-        'https://leagueoflegends.fandom.com/wikia.php?controller=UnifiedSearchSuggestions&method=getSuggestions&format=json&scope=internal&query=%s',
-    compFn: (res) => {
-        const data = JSON.parse(res.text) as CompResult;
-        return (
-            data.suggestions
-                // put suggestions with /LoL first since that's what we care about
-                .sort((a, b) => (a.endsWith('/LoL') ? -1 : b.endsWith('/LoL') ? 1 : 0))
-                .map((suggestion) => {
-                    const id = data.ids[suggestion];
-                    const serialized = suggestion.replace(/ /g, '_');
-                    const url = `https://leagueoflegends.fandom.com/wiki/${serialized}`;
+  alias: 'lol',
+  name: 'league of legends wiki',
+  searchUrl: 'https://leagueoflegends.fandom.com/wiki/Special:Search?scope=internal&query=%s',
+  compUrl:
+    'https://leagueoflegends.fandom.com/wikia.php?controller=UnifiedSearchSuggestions&method=getSuggestions&format=json&scope=internal&query=%s',
+  compFn: (res) => {
+    const data = JSON.parse(res.text) as CompResult;
+    return (
+      data.suggestions
+        // put suggestions with /LoL first since that's what we care about
+        .sort((a, b) => (a.endsWith('/LoL') ? -1 : b.endsWith('/LoL') ? 1 : 0))
+        .map((suggestion) => {
+          const id = data.ids[suggestion];
+          const serialized = suggestion.replace(/ /g, '_');
+          const url = `https://leagueoflegends.fandom.com/wiki/${serialized}`;
 
-                    const html = `
-                        <div style="display: flex; flex-direction: row">
-                            <img
-                                style="max-width: 160px; height: 90px; margin-right: 0.8em"
-                                alt="thumbnail"
-                                src="${data.images[id]}">
-                            <div>
-                                <div class="title">${suggestion}</div>
-                                <div>
-                                    <div class="url">${url}</div>
-                                </div>
-                            </div>
-                        </div>`;
+          const html = `
+            <div style="display: flex; flex-direction: row">
+                <img
+                    style="max-width: 160px; height: 90px; margin-right: 0.8em"
+                    alt="thumbnail"
+                    src="${data.images[id]}">
+                <div>
+                    <div class="title">${suggestion}</div>
+                    <div>
+                        <div class="url">${url}</div>
+                    </div>
+                </div>
+            </div>`;
 
-                    return { html, props: { url } };
-                })
-        );
-    },
-    faviconUrl: 'https://static.wikia.nocookie.net/leagueoflegends/images/4/4a/Site-favicon.ico',
+          return { html, props: { url } };
+        })
+    );
+  },
+  faviconUrl: 'https://static.wikia.nocookie.net/leagueoflegends/images/4/4a/Site-favicon.ico',
 };
 
 export default engine;
