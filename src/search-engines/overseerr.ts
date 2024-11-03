c17491545e637eeb0bcc37745a692ae6f99e6f33
William Huebner <wilhueb@gmail.com>
2024-11-03 02:55:18 -0500

    set default image for overseerr results

--- src/search-engines/overseerr.ts
+++ src/search-engines/overseerr.ts
@@ -8,7 +8,7 @@ type OverseerrCompResult = {
     id: number;
     mediaType: string;
     overview: string;
-    posterPath: string;
+    posterPath: string | null;
   } & ({ title: string; name: undefined } | { title: undefined; name: string }))[];
 };
 
@@ -19,7 +19,9 @@ const engine: SearchEngine = {
   compUrl: `${config.overseerrUrl}/api/v1/search?page=1&language=en&query=%s`,
   compFn: (res) =>
     (JSON.parse(res.text) as OverseerrCompResult).results.map((item) => {
-      const posterUrl = `https://image.tmdb.org/t/p/w600_and_h900_bestv2/${item.posterPath}`;
+      const posterUrl = item.posterPath
+        ? `https://image.tmdb.org/t/p/w600_and_h900_bestv2/${item.posterPath}`
+        : `${config.overseerrUrl}/images/overseerr_poster_not_found_logo_top.png`;
       const url = `${config.overseerrUrl}/${item.mediaType}/${item.id}`;
 
       const markup = searchResult({
