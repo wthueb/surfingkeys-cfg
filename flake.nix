4a3fcce413e2f6eff5fd5241e2af44b118d18b9f
William Huebner <wilhueb@gmail.com>
2025-11-28 23:05:07 -0500

    chore: nix flake

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      forAllSystems =
+        f:
+        nixpkgs.lib.genAttrs
+          [
+            "x86_64-linux"
+            "aarch64-linux"
+            "x86_64-darwin"
+            "aarch64-darwin"
+          ]
+          (
+            system:
+            f {
+              pkgs = nixpkgs.legacyPackages.${system};
+            }
+          );
+    in
+    {
+      devShells = forAllSystems (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              nodejs
+            ];
+          };
+        }
+      );
+    };
+}
